00196f73e430de3581ed9996cbc8dded7dcc753b
luolitao <luolitao@gmail.com>
2026-05-26 20:41:09 +0800

    fix spoof location

--- build/managed_components_list.temp.cmake
+++
@@ -1,3 +0,0 @@
-idf_build_component("C:/MyProjects/ESP/Remote-ID-Spoofer-main/managed_components/espressif__cJSON" "project_managed_components")
-idf_component_set_property(espressif__cJSON COMPONENT_VERSION "1.7.19~2")
-set(managed_components "espressif__cJSON")
